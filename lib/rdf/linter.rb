0cd03c1d7cee7f18fc0ba455e613c58a80a518e2
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-07-01 14:53:51 -0700

    Progress on linter parser simplification

--- lib/rdf/linter.rb
+++ lib/rdf/linter.rb
@@ -37,84 +37,38 @@ module RDF
 
       # Handle GET/POST /
       def linter
-        content_type, content = parse
-        if !params["raw"].to_s.empty?
-          puts "render distilled content as type #{content_type}"
-          status 200
-          headers "Allow" => "GET, POST", "Content-Type" => content_type
-          body content
-        else
-          @output = content unless content == @error
-          erubis :linter, :locals => {:title => "Structured Data Linter", :head => :linter}
-        end
-      end
-
-      # Return ordered accept mime-types
-      def accepts
-        types = []
-        request.env["HTTP_ACCEPT"].to_s.split(",").each do |type|
-          t, q = type.split(';q=')
-          q ||= t =~ /xml$/ ? "0.9" : "1"   # WebKit places application/xml at same priority as /html
-          types << [t, q.to_i]
-        end
-
-        types.sort {|a, b| b[1] <=> a[1]}.map {|(t, q)| t}
-      end
-
-      # Parse HTTP Accept header and find an suitable RDF writer
-      def writer(format = nil)
-        if format && format.to_sym != :accept
-          fmt = RDF::Format.for(format.to_sym)
-          return [format.to_sym, fmt.content_type.first] if fmt
-        end
-
-        # Look for formats matching accept headers
-        accepts.each do |t|
-          writer = RDF::Writer.for(:content_type => t)
-          return [writer.to_sym, t] if writer
-        end
-
-        return [:ntriples, "text/plain"]
-      end
-
-      # Format symbol for RDF formats
-      # @param [Symbol] reader_or_writer
-      # @return [Array<Symbol>] List of format symbols
-      def formats(reader_or_writer = nil)
-        # Symbols for different input formats
-        RDF::Format.select do |f|
-          reader_or_writer != :reader || f.reader
-          reader_or_writer != :writer || f.writer
-        end.map(&:to_sym).sort_by(&:to_s)
-      end
-
-      # Parse the an input file and re-serialize based on params and/or content-type/accept headers
-      def parse
+        params["in_fmt"] = "all" if params["in_fmt"].to_s.empty?
         reader_opts = {
           :prefixes => {},
           :base_uri => params["uri"],
-          :validate => params["validate"]
+          :validate => params["validate"],
+          :format   => params["in_fmt"].to_sym,
         }
-        reader_opts[:format] = params["in_fmt"].to_sym unless params["in_fmt"].nil? || params["in_fmt"] == 'content'
         reader_opts[:debug] = @debug = [] if params["debug"]
+        reader_opts[:tempfile] = params["datafile"] unless params["datafile"].to_s.empty?
+        reader_opts[:content] = params["content"] unless params["content"].to_s.empty?
+        
+        content_type, content = parse(reader_opts)
+        @output = content unless content == @error
+        erubis :linter, :locals => {:title => "Structured Data Linter", :head => :linter}
+      end
 
+      # Parse the an input file and re-serialize based on params and/or content-type/accept headers
+      def parse(reader_opts)
         graph = RDF::Graph.new
-        format = params["in_fmt"].to_sym if params["in_fmt"]
+        format = reader_opts[:format]
 
         case
-        when !params["datafile"].to_s.empty?
-          raise RDF::ReaderError, "Specify input format" if format.nil? || format == :content
-          puts "Open datafile with format #{format}"
-          tempfile =
-          reader = RDF::Reader.for(format).new(tempfile, reader_opts) {|r| graph << r}
-        when !params["content"].to_s.empty?
-          raise RDF::ReaderError, "Specify input format" if format.nil? || format == :content
-          puts "Open form data with format #{format}"
-          @content = params["content"]
+        when reader_opts[:tempfile]
+          puts "Parse input file #{reader_opts[:tempfile].inspect} with format #{format}"
+          reader = RDF::Reader.for(format).new(reader_opts[:tempfile], reader_opts) {|r| graph << r}
+        when  reader_opts[:content]
+          puts "Parse form data with format #{format}"
+          @content = reader_opts[:content]
           reader = RDF::Reader.for(format).new(@content, reader_opts) {|r| graph << r}
-        when !params["uri"].to_s.empty?
-          puts "Open uri <#{params["uri"]}> with format #{format}"
-          reader = RDF::Reader.open(params["uri"], reader_opts) {|r| graph << r}
+        when reader_opts[:base_uri]
+          puts "Open uri <#{reader_opts[:base_uri]}> with format #{format}"
+          reader = RDF::Reader.open(reader_opts[:base_uri], reader_opts) {|r| graph << r}
           params["in_fmt"] = reader.class.to_sym if format.nil? || format == :content
         else
           return ["text/html", ""]
@@ -131,30 +85,12 @@ module RDF
       rescue RDF::ReaderError => e
         @error = "RDF::ReaderError: #{e.message}"
         puts @error  # to log
-        content_type ||= accepts.first
-        case content_type
-        when /html/
-          [content_type, @error] # XXX
-        when /xml/
-          [content_type, @error.to_xml]
-        else
-          [content_type, @error]
-        end
+        [content_type, @error] # XXX
       rescue
         raise unless settings.environment == :production
         @error = "#{$!.class}: #{$!.message}"
         puts @error  # to log
-        content_type ||= "text/html" if accepts.include?("text/html")
-        content_type ||= "application/xml" if accepts.include?("application/xml")
-        content_type ||= "text/plain"
-        case content_type
-        when "application/xml"
-          [content_type, @error.to_xml]
-        when "text/html"
-          [content_type, @error] # XXX
-        else
-          ["text/plain", @error]
-        end
+        ["text/html", @error] # XXX
       end
     end
   end
