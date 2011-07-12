b53613542732c0360d58fdc73c47cd89902432f7
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-07-11 23:15:38 -0700

    Collect and show statistics on parsers used.

--- lib/rdf/linter/parser.rb
+++ lib/rdf/linter/parser.rb
@@ -8,7 +8,7 @@ module RDF::Linter
       graph = RDF::Graph.new
       format = reader_opts[:format]
 
-      case
+      reader = case
       when reader_opts[:tempfile]
         RDF::Reader.for(format).new(reader_opts[:tempfile], reader_opts) {|r| graph << r}
       when  reader_opts[:content]
@@ -20,6 +20,13 @@ module RDF::Linter
         return ["text/html", ""]
       end
 
+      @parsed_statements = case reader
+      when RDF::All::Reader
+        reader.statement_count
+      else
+        {reader.class => graph.size }
+      end
+      
       writer_opts = reader_opts
       writer_opts = reader_opts.merge(
         :standard_prefixes => true,
