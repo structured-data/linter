d604961c499f421fb6ead0175d71bea07ef0cb0a
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-07-28 14:49:56 -0700

    Fix sioc_post_comment property_value to all interpolation.

--- lib/rdf/linter/snippets/sioc_post_comment.rb
+++ lib/rdf/linter/snippets/sioc_post_comment.rb
@@ -15,7 +15,7 @@ module RDF::Linter
         "http://purl.org/dc/terms/date"
       ],
       :description_props => ["http://purl.org/rss/1.0/modules/content/encoded"],
-      :property_value => %q(
+      :property_value => %(
       - if res = yield(object)
         != res
       - elsif object.literal? && object.datatype == "http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral"
